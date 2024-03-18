2a338b94dcabce50fe76559ebf07b304904b9d23
Svixdream <Svixdream@gmail.com>
2024-03-18 17:56:58 +0300

    feat: Switch it Up!

---
+++ 8kyu/switch_it_Up.js
@@ -0,0 +1,12 @@
+/* When provided with a number between 0-9, return it in words.
+
+Input :: 1
+
+Output :: "One".
+
+If your language supports it, try using a switch statement. */
+
+function switchItUp(number){
+    const o = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+    return o [number]
+    }
